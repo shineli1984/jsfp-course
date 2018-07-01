faf49f97c42bef8cff3874b81f92f0a1998b79d1
Robert Chang <cht8687@gmail.com>
2018-07-02 01:09:47 +1000

    practise

--- week1/review.js
+++ week1/review.js
@@ -1,81 +1,110 @@
-import {compose, curry} from 'ramda'
+import {
+	compose,
+	curry,
+	path,
+	join,
+	flip,
+	__,
+	append,
+  evolve,
+  applySpec,
+  length,
+  concat
+} from 'ramda'
+import assert from 'assert';
 /**
  * Review last week's contents: curry & compose through various composition of lambda functions
  */
 
 const data1 = {
-  a: {
-    b: [
-      'content',
-      'under',
-      'b'
-    ]
-  }
+	a: {
+		b: [
+			'content',
+			'under',
+			'b'
+		]
+	}
 }
 
-// getContentUnderB(data1) === 'content under b'
 const getContentUnderB = compose(
-  // ...
+	join(' '),
+	path(['a', 'b'])
 )
+
+assert.ok(getContentUnderB(data1) === 'content under b')
 
 // ----------------
 const f1 = (a, b, c) => a * b + c
 const f1p = compose(
-  // ...
+	flip
 )(f1)
-// flipping first and second arguments:
-// f1(1, 2, 3) === f1p(2, 1, 3)
 
+// flipping first and second arguments:
+assert.ok(f1(1, 2, 3) === f1p(2, 1, 3))
 
 // ----------------
 const f2 = curry((a, b, c) => a * b + c)
-const f2p = f2(
-  // ...
-)
+const f2p = (a, b, c) => f2(a, __, b)(c);
 // flipping second and third arguments
-// f2(1, 2, 3) === f2p(1, 3, 2)
+assert.ok(f2(1, 2, 3) === f2p(1, 3, 2))
 
 // ----------------
 const data2 = {
-  a: {
-    b: {
-      c: [1]
-    }
-  },
-  d: {
-    e: [2]
-  }
+	a: {
+		b: {
+			c: [1]
+		}
+	},
+	d: {
+		e: [2]
+	}
 }
-// append2ToC3ToE(data2) ===
-// {
-//   a: {
-//     b: {
-//       c: [1, 2]
-//     }
-//   },
-//   d: {
-//     e: [2, 3]
-//   }
-// }
-//
-const append2ToC3ToE = compose(
-  // ...
-)
 
+const append2ToC3ToE = evolve({
+	a: {
+		b: {
+			c: append(2)
+		}
+	},
+	d: {
+		e: append(3)
+	}
+})
+// assert.ok(append2ToC3ToE(data2) === {
+// 	a: {
+// 		b: {
+// 			c: [1, 2]
+// 		}
+// 	},
+// 	d: {
+// 		e: [2, 3]
+// 	}
+// })
 
 // --------------
-// stats('10', '-2') ===
-// {
-//   totalCharacters: 3
-//   arg1: {
-//     positive: true
-//   },
-//   arg2: {
-//     positive: false
-//   }
-// }
-//
-const stats = undefined//..
 
+const stats = applySpec({
+  totalCharacters: (a, b) => length(concat(a,b)),
+  arg1: {
+    positive: (a, _) => a>0
+  },
+  arg2: {
+    positive: (_, b) => b>0
+  }
+})
+
+console.log(stats('10', '-2'))
+
+assert.ok(stats('10', '-2') ===
+{
+  totalCharacters: 4,
+  arg1: {
+    positive: true
+  },
+  arg2: {
+    positive: false
+  }
+}
+)
 
 // touch on pointfree

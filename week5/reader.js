34ad3f015676549e81cc8cd8c0d2b63bc2804786
Shine Li <fcpga_te@icloud.com>
2018-08-12 16:34:33 +1000

    add comments and fix typo

--- week5/reader.js
+++ week5/reader.js
@@ -6,15 +6,18 @@ const Reader = daggy.tagged('Reader', ['run'])
 Reader.ask = Reader(identity)
 Reader.of = a => Reader(always(a))
 
+// map :: Reader e a ~> (a -> b) -> Reader e b
 Reader.prototype.map = function(f) {
   return Reader(e =>
     f(this.run(e))
   )
 }
 
+// map :: Reader e a ~> (a -> Reader e b) -> Reader e b
 Reader.prototype.chain = function(f) {
-  return Reader((e) => f(this.run(e)).run(e));
+  return Reader(e =>
+    f(this.run(e))
+      .run(e));
 };
 
 export default Reader
-
--- week5/writerExample.js
+++ week5/writerExample.js
@@ -2,14 +2,14 @@ import Writer from './writer'
 import {adjust, concat, compose, composeK, always} from 'ramda'
 
 export const add1 = a => a + 1
-export const multipleBy2 = a => a * 2
+export const multiplyBy2 = a => a * 2
 export const divideBy3 = a => a / 3
 
 // it's easy to compose the above:
 compose(
   divideBy3,
   add1,
-  multipleBy2
+  multiplyBy2
 )
 
 // when we want to log, this is NOT the way to do it:
@@ -18,8 +18,8 @@ compose(
 const add1WithLog = a =>
   [add1(a), `${a} addBy1\n`]
 
-const multipleBy2WithLog = a =>
-  [multipleBy2(a), `${a} multipleBy2\n`]
+const multiplyBy2WithLog = a =>
+  [multiplyBy2(a), `${a} multiplyBy2\n`]
 
 const divideBy3WithLog = a =>
   [divideBy3(a), `${a} divideBy3\n`]
@@ -37,10 +37,10 @@ const operationsWithLog = compose(
     1,
     add1WithLog(a)
   ),
-  multipleBy2WithLog
+  multiplyBy2WithLog
 )
 
-console.log('Options 2', operationsWithLog(1).toString())
+console.log('Options 1', operationsWithLog(1).toString())
 
 // What happened above can be broken down into the following parts:
 // 1. calling of function like add1WithLog, which returns a tuple([] with 2 elements here)
@@ -64,31 +64,31 @@ const W = Writer(Array)
 // then we turn functions:
 //   divideBy3,
 //   add1,
-//   multipleBy2
+//   multiplyBy2
 // to output a writer instead.
-// That is to turn Int -> Int to Int -> Writer Int
-const multipleBy2WithWriter = a =>
+// That is to turn Int -> Int to Int -> Writer [String] Int
+const multiplyBy2WithWriter = a =>
   W
     .of(a)
-    .tell(`${a} multipleBy2`)
-    .map(always(multipleBy2(a)))
+    .tell([`${a} multiplyBy2`])
+    .map(always(multiplyBy2(a)))
 
 const add1WithWriter = a =>
   W
     .of(a)
-    .tell(`${a} add1`)
+    .tell([`${a} add1`])
     .map(always(add1(a)))
 
 const divideBy3WithWriter = a =>
   W
     .of(a)
-    .tell(`${a} divideBy3`)
+    .tell([`${a} divideBy3`])
     .map(always(divideBy3(a)))
 
 const operationsWithWriter = composeK(
   divideBy3WithWriter,
   add1WithWriter,
-  multipleBy2WithWriter
+  multiplyBy2WithWriter
 )
 
-console.log('Option 2: ', operationsWithWriter(1).run().toString())
+console.log('Option 2: ', operationsWithWriter(1).run())
